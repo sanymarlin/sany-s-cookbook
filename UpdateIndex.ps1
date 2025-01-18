6731ef6ad6fadcff7d0106f433fd0e4ef11265a1
sanymarlin <sany.marlin@proton.me>
2025-01-18 13:54:03 +0100

    Automated update of the index of recipes (in index.html) now working.

--- UpdateIndex.ps1
+++ UpdateIndex.ps1
@@ -42,27 +42,50 @@ if ($Language -eq "fr") {
 # Read the HTML File with UTF-8 Encoding
 $IndexContent = Get-Content -Path $IndexFilePath -Raw -Encoding UTF8
 
-# Find the Target Section
+# Locate the Target Section
 $SectionStart = $IndexContent.IndexOf("<div class=`"recipe-list $SectionTag`">")
 if ($SectionStart -eq -1) {
     Write-Error "Target section for language '$Language' not found in the index.html file."
     exit 1
 }
 
-# Insert New Recipe in the Section
-$InsertPoint = $IndexContent.IndexOf("</ul>", $SectionStart)
-if ($InsertPoint -eq -1) {
-    Write-Error "Could not find the end of the target section's <ul> in the index.html file."
+$UlStart = $IndexContent.IndexOf("<ul>", $SectionStart)
+$UlEnd = $IndexContent.IndexOf("</ul>", $UlStart)
+if ($UlStart -eq -1 -or $UlEnd -eq -1) {
+    Write-Error "Could not find the <ul> tags in the target section."
     exit 1
 }
 
-# Create the New Recipe Tag
-$NewRecipeTag = "    <li><a href=`"Recipes/$RecipeFileName`">$DisplayTitle</a></li>"
+# Extract Current Recipes
+$UlContent = $IndexContent.Substring($UlStart + 4, $UlEnd - $UlStart - 4).Trim()
+$CurrentRecipes = @()
 
-# Insert the New Recipe Tag
-$UpdatedContent = $IndexContent.Substring(0, $InsertPoint) + "`n$NewRecipeTag`n" + $IndexContent.Substring($InsertPoint)
+if ($UlContent) {
+    $CurrentRecipes = $UlContent -split "`n" | ForEach-Object {
+        $_.Trim()
+    }
+}
+
+# Add New Recipe
+$NewRecipeTag = "<li><a href=`"Recipes/$RecipeFileName`">$DisplayTitle</a></li>"
+$CurrentRecipes += $NewRecipeTag
+
+# Sort Recipes Alphabetically by Display Title
+$SortedRecipes = $CurrentRecipes | Sort-Object {
+    if ($_ -match ">(.*?)</a>") {
+        $Matches[1]
+    } else {
+        $_
+    }
+}
+
+# Reconstruct the <ul> Section
+$UpdatedUlContent = "<ul>`n    " + ($SortedRecipes -join "`n    ") + "`n</ul>"
+
+# Replace the <ul> Section in the Original Content
+$UpdatedContent = $IndexContent.Substring(0, $UlStart + 4) + "`n    " + $UpdatedUlContent + "`n" + $IndexContent.Substring($UlEnd)
 
 # Save the Updated File with UTF-8 Encoding
 Set-Content -Path $IndexFilePath -Value $UpdatedContent -Encoding UTF8
 
-Write-Output "Successfully added recipe '$DisplayTitle' to the $Language section."
+Write-Output "Successfully added recipe '$DisplayTitle' to the $Language section in alphabetical order."
